6fe355ccc97879305620862106d5444b5603533a
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2025-04-01 11:46:46 +0200

    commit 1

--- script/script.js
+++ script/script.js
@@ -21,3 +21,40 @@ const users = [
 ];
 
 // hieronder komt jullie code - veel succes
+let txtname = document.querySelector("#name");
+let txtemail = document.querySelector("#email");
+let txtpassword = document.querySelector("#password");
+const btnregister = document.querySelector("#registreer");
+
+const filter = document.querySelector("#filter");
+const btnfilter = document.querySelector("#btnfilter");
+
+const txtlijst = document.querySelector(".lijst");
+
+const addLogin = (fullname, email, password) => {
+  const newUser = {
+    fullname,
+    email,
+    password,
+  };
+  users.push(newUser);
+  txtname = "";
+  txtemail = "";
+  txtpassword = "";
+};
+const addLijst = (fullname, email) => {
+  users.forEach((gebruiker) => {
+    const nieuwlijstnaam = document.createElement("p");
+    const nieuwlijstemail = document.createElement("p");
+    nieuwlijstnaam.innerHTML = gebruiker[fullname];
+    nieuwlijstemail.innerHTML = gebruiker[email];
+    txtlijst.appendChild(nieuwlijstemail);
+    txtlijst.appendChild(nieuwlijstnaam);
+  });
+};
+
+btnregister.addEventListener("click", (e) => {
+  addLogin(txtname.value, txtemail.value, txtpassword.value);
+  addLijst(users.fullname, users.email);
+  console.log(users);
+});
